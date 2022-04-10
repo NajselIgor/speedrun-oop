0f99dc6eaeab2f14747875fe32e566d075895627
xnajsel <xnajsel@stuba.sk>
2022-04-10 22:47:14 +0200

    try catch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,34 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String args[]) {
-        ArrayList<Zviera> zvery = new ArrayList<>();
+//        ArrayList<Zviera> zvery = new ArrayList<>();
+//
+//        zvery.add(new Pes("bruno"));
+//        zvery.add(new Macka("pica"));
+//
+//        for (Zviera zver: zvery) {
+//            System.out.println("pred");
+//            try {
+//                System.out.println("pred try");
+//                kontrola(zver);
+//                System.out.println("po try");
+//            } catch (NotPesExeption e) {
+//                System.out.println("catch");
+//                System.out.println(e.getMessage());
+//            } catch (NotMackaExeption e) {
+//                System.out.println("catch");
+//                System.out.println(e.getMessage());
+//            } finally {
+//                System.out.println("finaly");
+//            }
+//
+//            System.out.println("po");
+//            System.out.println("===========");
+//        }
+        List<Zviera> zvery = new ArrayList<>(List.of(new Pes("bruno"), new Macka("pica")));
 
-        zvery.add(new Pes("bruno"));
-        zvery.add(new Macka("pica"));
-
-        for (Zviera zver: zvery) {
-            System.out.println("pred");
-            try {
-                System.out.println("pred try");
-                kontrola(zver);
-                System.out.println("po try");
-            } catch (NotPesExeption e) {
-                System.out.println("catch");
-                System.out.println(e.getMessage());
-            } catch (NotMackaExeption e) {
-                System.out.println("catch");
-                System.out.println(e.getMessage());
-            } finally {
-                System.out.println("finaly");
-            }
-
-            System.out.println("po");
-            System.out.println("===========");
-        }
     }
 
     private static void kontrola(Zviera zver) throws NotPesExeption, NotMackaExeption {
