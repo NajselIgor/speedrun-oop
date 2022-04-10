1bb0fd07444a1bb3f87c412ef0273f0e9a5af1c7
xnajsel <xnajsel@stuba.sk>
2022-04-10 22:59:41 +0200

    mapy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String args[]) {
@@ -29,6 +31,32 @@ public class Main {
 //        }
         List<Zviera> zvery = new ArrayList<>(List.of(new Pes("bruno"), new Macka("pica")));
 
+        Map<String, String> hlavneMesta = new HashMap<>();
+
+        hlavneMesta.put("Slovensko", "Bratislava");
+        hlavneMesta.put("Cesko", "Praha");
+        hlavneMesta.put("Polsko", "Varsava");
+        hlavneMesta.put("Madarsko", "Budapest");
+
+        System.out.println(hlavneMesta.size());
+        if(hlavneMesta.containsKey("Slovensko")){
+            System.out.println(hlavneMesta.get("Slovensko"));
+            hlavneMesta.remove("Slovensko");
+        }
+
+        for(String kluc : hlavneMesta.keySet()) {
+            System.out.println(kluc);
+            System.out.println(hlavneMesta.get(kluc));
+        }
+        System.out.println("UUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+        for(String hodnota : hlavneMesta.values()) {
+            System.out.println(hodnota);
+        }
+
+        System.out.println(hlavneMesta.get("Slovensko"));
+
+        hlavneMesta.clear();
+        System.out.println(hlavneMesta);
     }
 
     private static void kontrola(Zviera zver) throws NotPesExeption, NotMackaExeption {
